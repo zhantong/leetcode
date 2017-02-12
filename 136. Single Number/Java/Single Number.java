efe296365353810bf54b99851f00a37631065e47
Tong ZHAN <zhantong1994@163.com>
2017-02-12 23:09:52 +0800

    136. Single Number

---
+++ 136. Single Number/Java/Single Number.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
---
+++ 136. Single Number/Python/Single Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+        return result ^ 0
