4550ce6f16dda936c7405e9c3c445ae639ca8232
Tong ZHAN <zhantong1994@163.com>
2017-02-16 20:42:32 +0800

    326. Power of Three

---
+++ 326. Power of Three/Python/Power of Three.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        return (3 ** 19) % n == 0
