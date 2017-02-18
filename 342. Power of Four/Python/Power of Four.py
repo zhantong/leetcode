5143434decb8be208856def7ca39070910431439
Tong ZHAN <zhantong1994@163.com>
2017-02-18 22:58:25 +0800

    342. Power of Four

---
+++ 342. Power of Four/Python/Power of Four.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return num > 0 and num & (num - 1) == 0 and num & 0x55555555 == num
