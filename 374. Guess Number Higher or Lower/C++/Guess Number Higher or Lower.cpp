b294f17e9f3118ef46428cc9eb2cdba54eee314f
Tong ZHAN <zhantong1994@163.com>
2017-02-20 23:21:35 +0800

    374. Guess Number Higher or Lower

---
+++ 374. Guess Number Higher or Lower/C++/Guess Number Higher or Lower.cpp
@@ -0,0 +1,25 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long low = 1;
+        long high = n;
+        while (low <= high) {
+            int mid = (int) ((low + high) / 2);
+            int guessResult = guess(mid);
+            if (guessResult == 0) {
+                return mid;
+            }
+            if (guessResult == 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 0;
+    }
+};
---
+++ 374. Guess Number Higher or Lower/Python/Guess Number Higher or Lower.py
@@ -0,0 +1,22 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low + high) // 2
+            guess_result = guess(mid)
+            if guess_result == 0:
+                return mid
+            if guess_result == 1:
+                low = mid + 1
+            else:
+                high = mid - 1
