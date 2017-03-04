840fe81a65f01e6938f8e55d2ee154f8e33dd810
Tong ZHAN <zhantong1994@163.com>
2017-03-04 23:12:53 +0800

    319. Bulb Switcher

---
+++ 319. Bulb Switcher/Java/Bulb Switcher.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
---
+++ 319. Bulb Switcher/Python/Bulb Switcher.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(n ** 0.5)
