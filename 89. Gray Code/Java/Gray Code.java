f9b091cdfa6a2273177b835ad3c14b0684593f34
Tong ZHAN <zhantong1994@163.com>
2017-03-06 19:43:41 +0800

    89. Gray Code

---
+++ 89. Gray Code/Java/Gray Code.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 1 << n; i++) {
+            result.add(i ^ (i >> 1));
+        }
+        return result;
+    }
+}
---
+++ 89. Gray Code/Python/Gray Code.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        return [i ^ (i >> 1) for i in range(1 << n)]
