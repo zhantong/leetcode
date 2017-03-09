8043dce5937057097ea6e232b382d5a24dc33307
Tong ZHAN <zhantong1994@163.com>
2017-03-09 21:49:12 +0800

    300. Longest Increasing Subsequence

---
+++ 300. Longest Increasing Subsequence/C++/Longest Increasing Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int> &nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        vector<int> dp(nums.size(), 1);
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
