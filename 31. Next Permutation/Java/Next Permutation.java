5c05c2ef169ac0de18c2ee124d9b12c82df0e0be
Tong ZHAN <zhantong1994@163.com>
2017-03-30 23:14:14 +0800

    31. Next Permutation

---
+++ 31. Next Permutation/Java/Next Permutation.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            Arrays.sort(nums);
+            return;
+        }
+        int j = nums.length - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        Arrays.sort(nums, i + 1, nums.length);
+    }
+}
---
+++ 31. Next Permutation/Python/Next Permutation.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i < 0:
+            nums.sort()
+            return
+        j = len(nums) - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = sorted(nums[i + 1:])
