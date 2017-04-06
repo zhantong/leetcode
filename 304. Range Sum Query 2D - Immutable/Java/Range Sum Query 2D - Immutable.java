265514d1befecfcc225a4a50c45fe33741e78f8f
Tong ZHAN <zhantong1994@163.com>
2017-04-06 22:06:28 +0800

    304. Range Sum Query 2D - Immutable

---
+++ 304. Range Sum Query 2D - Immutable/Java/Range Sum Query 2D - Immutable.java
@@ -0,0 +1,27 @@
+public class NumMatrix {
+    int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0) {
+            return;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        sums = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sums[i][j] = matrix[i - 1][j - 1] + sums[i][j - 1] + sums[i - 1][j] - sums[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
---
+++ 304. Range Sum Query 2D - Immutable/Python/Range Sum Query 2D - Immutable.py
@@ -0,0 +1,31 @@
+class NumMatrix(object):
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        if not matrix:
+            return
+        m = len(matrix)
+        n = len(matrix[0])
+        self.sums = [[0] * (n + 1) for i in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                self.sums[i][j] = matrix[i - 1][j - 1] + self.sums[i][j - 1] + self.sums[i - 1][j] - \
+                                  self.sums[i - 1][j - 1]
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return self.sums[row2 + 1][col2 + 1] - self.sums[row1][col2 + 1] - self.sums[row2 + 1][col1] + \
+               self.sums[row1][col1]
+
+
+
+        # Your NumMatrix object will be instantiated and called as such:
+        # obj = NumMatrix(matrix)
+        # param_1 = obj.sumRegion(row1,col1,row2,col2)
