202ab1b7ea5d00137eefbbc1499c1bc15302be09
Tong ZHAN <zhantong1994@163.com>
2017-04-28 23:13:58 +0800

    391. Perfect Rectangle

---
+++ 391. Perfect Rectangle/Java/Perfect Rectangle.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public boolean isRectangleCover(int[][] rectangles) {
+        int x1 = Integer.MAX_VALUE;
+        int y1 = Integer.MAX_VALUE;
+        int x2 = Integer.MIN_VALUE;
+        int y2 = Integer.MIN_VALUE;
+        Set<String> got = new HashSet<>();
+        int sumUp = 0;
+        for (int[] rectangle : rectangles) {
+            int a1 = rectangle[0];
+            int b1 = rectangle[1];
+            int a2 = rectangle[2];
+            int b2 = rectangle[3];
+            x1 = Math.min(x1, a1);
+            y1 = Math.min(y1, b1);
+            x2 = Math.max(x2, a2);
+            y2 = Math.max(y2, b2);
+            sumUp += (a2 - a1) * (b2 - b1);
+            for (int i = 0; i < 3; i += 2) {
+                for (int j = 1; j < 4; j += 2) {
+                    String key = rectangle[i] + " " + rectangle[j];
+                    if (got.contains(key)) {
+                        got.remove(key);
+                    } else {
+                        got.add(key);
+                    }
+                }
+            }
+        }
+        return got.size() == 4
+                && got.contains(x1 + " " + y1)
+                && got.contains(x1 + " " + y2)
+                && got.contains(x2 + " " + y1)
+                && got.contains(x2 + " " + y2)
+                && sumUp == (x2 - x1) * (y2 - y1);
+    }
+}
---
+++ 391. Perfect Rectangle/Python/Perfect Rectangle.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def isRectangleCover(self, rectangles):
+        """
+        :type rectangles: List[List[int]]
+        :rtype: bool
+        """
+        x1 = sys.maxsize
+        y1 = sys.maxsize
+        x2 = -sys.maxsize
+        y2 = -sys.maxsize
+        got = set()
+        sum_up = 0
+        for rectangle in rectangles:
+            a1 = rectangle[0]
+            b1 = rectangle[1]
+            a2 = rectangle[2]
+            b2 = rectangle[3]
+            x1 = min(x1, a1)
+            y1 = min(y1, b1)
+            x2 = max(x2, a2)
+            y2 = max(y2, b2)
+            sum_up += (a2 - a1) * (b2 - b1)
+            for i in [0, 2]:
+                for j in [1, 3]:
+                    key = str(rectangle[i]) + ' ' + str(rectangle[j])
+                    if key in got:
+                        got.remove(key)
+                    else:
+                        got.add(key)
+        return len(got) == 4 \
+               and (str(x1) + ' ' + str(y1)) in got \
+               and (str(x1) + ' ' + str(y2)) in got \
+               and (str(x2) + ' ' + str(y1)) in got \
+               and (str(x2) + ' ' + str(y2)) in got \
+               and sum_up == (x2 - x1) * (y2 - y1)
