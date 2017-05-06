57e577e28e4eacaf9a982e663aba467543cabdf7
Tong ZHAN <zhantong1994@163.com>
2017-05-06 20:46:53 +0800

    460. LFU Cache

---
+++ 460. LFU Cache/C++/LFU Cache.cpp
@@ -0,0 +1,116 @@
+class LFUCache {
+public:
+    class Node {
+    public:
+        int key;
+        int value;
+        int times;
+        Node *prev;
+        Node *next;
+
+        Node(int key, int value, int times) {
+            this->key = key;
+            this->value = value;
+            this->times = times;
+        }
+    };
+
+    int initCapacity;
+    int capacity;
+    unordered_map<int, Node *> memoryNodes;
+    unordered_map<int, Node *> memoryTimes;
+    Node *head;
+    Node *tail;
+
+    LFUCache(int capacity) {
+        initCapacity = capacity;
+        this->capacity = capacity;
+        head = new Node(-1, -1, -1);
+        tail = new Node(-1, -1, -1);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (memoryNodes.find(key) == memoryNodes.end()) {
+            return -1;
+        }
+        int value = memoryNodes[key]->value;
+        put(key, value);
+        return value;
+    }
+
+    void put(int key, int value) {
+        if (initCapacity == 0) {
+            return;
+        }
+        if (memoryNodes.find(key) != memoryNodes.end()) {
+            Node *node = memoryNodes[key];
+            node->value = value;
+            if (memoryTimes.find(node->times + 1) != memoryTimes.end()) {
+                if (memoryTimes[node->times] == node) {
+                    if (node->next->times == node->times) {
+                        memoryTimes[node->times] = node->next;
+                    } else {
+                        memoryTimes.erase(node->times);
+                    }
+                }
+                node->prev->next = node->next;
+                node->next->prev = node->prev;
+                node->prev = memoryTimes[node->times + 1]->prev;
+                node->next = memoryTimes[node->times + 1];
+                node->next->prev = node;
+                node->prev->next = node;
+                memoryTimes[node->times + 1] = node;
+            } else {
+                if (memoryTimes[node->times] != node) {
+                    node->prev->next = node->next;
+                    node->next->prev = node->prev;
+                    node->prev = memoryTimes[node->times]->prev;
+                    node->next = memoryTimes[node->times];
+                    node->next->prev = node;
+                    node->prev->next = node;
+                } else {
+                    if (node->next->times == node->times) {
+                        memoryTimes[node->times] = node->next;
+                    } else {
+                        memoryTimes.erase(node->times);
+                    }
+                }
+                memoryTimes[node->times + 1] = node;
+            }
+            node->times++;
+        } else {
+            Node *node = new Node(key, value, 1);
+            memoryNodes[key] = node;
+            if (capacity == 0) {
+                Node *nodeToRemove = tail->prev;
+                if (memoryTimes[nodeToRemove->times] == nodeToRemove) {
+                    memoryTimes.erase(nodeToRemove->times);
+                }
+                nodeToRemove->prev->next = nodeToRemove->next;
+                nodeToRemove->next->prev = nodeToRemove->prev;
+                memoryNodes.erase(nodeToRemove->key);
+                capacity++;
+            }
+            if (memoryTimes.find(1) != memoryTimes.end()) {
+                node->prev = memoryTimes[1]->prev;
+                node->next = memoryTimes[1];
+            } else {
+                node->prev = tail->prev;
+                node->next = tail;
+            }
+            node->prev->next = node;
+            node->next->prev = node;
+            memoryTimes[1] = node;
+            capacity--;
+        }
+    }
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
---
+++ 460. LFU Cache/Python/LFU Cache.py
@@ -0,0 +1,101 @@
+class LFUCache(object):
+    class Node:
+        def __init__(self, key, value, times):
+            self.key = key
+            self.value = value
+            self.times = times
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.init_capacity = capacity
+        self.capacity = capacity
+        self.memory_nodes = {}
+        self.memory_times = {}
+        self.head = self.Node(-1, -1, -1)
+        self.tail = self.Node(-1, -1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.memory_nodes:
+            return -1
+        self.put(key, self.memory_nodes[key].value)
+        return self.memory_nodes[key].value
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if self.init_capacity == 0:
+            return
+        if key in self.memory_nodes:
+            node = self.memory_nodes[key]
+            node.value = value
+
+            if node.times + 1 in self.memory_times:
+                if self.memory_times[node.times] == node:
+                    if node.next.times == node.times:
+                        self.memory_times[node.times] = node.next
+                    else:
+                        del self.memory_times[node.times]
+                node.prev.next = node.next
+                node.next.prev = node.prev
+                node.prev = self.memory_times[node.times + 1].prev
+                node.next = self.memory_times[node.times + 1]
+                node.next.prev = node
+                node.prev.next = node
+                self.memory_times[node.times + 1] = node
+            else:
+                if self.memory_times[node.times] != node:
+                    node.prev.next = node.next
+                    node.next.prev = node.prev
+                    node.prev = self.memory_times[node.times].prev
+                    node.next = self.memory_times[node.times]
+                    node.next.prev = node
+                    node.prev.next = node
+                else:
+                    if node.next.times == node.times:
+                        self.memory_times[node.times] = node.next
+                    else:
+                        del self.memory_times[node.times]
+                self.memory_times[node.times + 1] = node
+            node.times += 1
+        else:
+            node = self.Node(key, value, 1)
+            self.memory_nodes[key] = node
+            if self.capacity == 0:
+                node_to_remove = self.tail.prev
+                if self.memory_times[node_to_remove.times] == node_to_remove:
+                    del self.memory_times[node_to_remove.times]
+                node_to_remove.prev.next = node_to_remove.next
+                node_to_remove.next.prev = node_to_remove.prev
+                del self.memory_nodes[node_to_remove.key]
+                self.capacity += 1
+            if 1 in self.memory_times:
+                node.prev = self.memory_times[1].prev
+                node.next = self.memory_times[1]
+
+            else:
+                node.prev = self.tail.prev
+                node.next = self.tail
+            node.next.prev = node
+            node.prev.next = node
+            self.memory_times[1] = node
+            self.capacity -= 1
+
+
+
+            # Your LFUCache object will be instantiated and called as such:
+            # obj = LFUCache(capacity)
+            # param_1 = obj.get(key)
+            # obj.put(key,value)
