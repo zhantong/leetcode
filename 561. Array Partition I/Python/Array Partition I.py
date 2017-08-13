0007b58ac1ba29d6c4933a6759f1548eeb9bd02a
zhantong <zhantong1994@163.com>
2017-08-13 23:54:04 +0800

    561. Array Partition I

---
+++ 561. Array Partition I/Python/Array Partition I.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return sum(nums[::2])
