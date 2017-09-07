6990f85739514a905e3c38af43ae27b55ac9b941
Tong ZHAN <zhantong1994@163.com>
2017-09-07 23:05:10 +0800

    657. Judge Route Circle

---
+++ 657. Judge Route Circle/C++/Judge Route Circle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        if (moves.length() % 2 != 0) {
+            return false;
+        }
+        int countV = 0;
+        int countH = 0;
+        for (auto &letter : moves) {
+            switch (letter) {
+                case 'U':
+                    countV++;
+                    break;
+                case 'D':
+                    countV--;
+                    break;
+                case 'L':
+                    countH++;
+                    break;
+                case 'R':
+                    countH--;
+                    break;
+            }
+        }
+        return countV == 0 && countH == 0;
+    }
+};
---
+++ 657. Judge Route Circle/Java/Judge Route Circle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        if (moves.length() % 2 != 0) {
+            return false;
+        }
+        int countV = 0;
+        int countH = 0;
+        for (char letter : moves.toCharArray()) {
+            switch (letter) {
+                case 'U':
+                    countV++;
+                    break;
+                case 'D':
+                    countV--;
+                    break;
+                case 'L':
+                    countH++;
+                    break;
+                case 'R':
+                    countH--;
+                    break;
+            }
+        }
+        return countV == 0 && countH == 0;
+    }
+}
