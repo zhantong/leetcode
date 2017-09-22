d2b41ab656b5ccfc24589b22c28da3f0dfc2ec78
Tong ZHAN <zhantong1994@163.com>
2017-09-22 15:21:14 +0800

    650. 2 Keys Keyboard

---
+++ 650. 2 Keys Keyboard/C++/2 Keys Keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n + 1, 0);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = i / 2; j > 1; j--) {
+                if (i % j == 0) {
+                    dp[i] = dp[j] + i / j;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
