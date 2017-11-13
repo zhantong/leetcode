64f9980e28fdfbfdebf2e8075f092a9b4625c04e
Tong ZHAN <zhantong1994@163.com>
2017-11-13 10:09:37 +0800

    更新100. Same Tree

--- 100. Same Tree/Java/Solution.java
+++ 100. Same Tree/Java/Solution.java
@@ -9,7 +9,8 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if ((p == null && q == null) || (p != null && q != null && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))) {
+        if ((p == null && q == null) ||
+        (p != null && q != null && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))) {
             return true;
         }
         return false;
